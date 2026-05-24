711f514c79ce57ba7c6e9664ade17a0c3ea971e7
Jack Pyburn <jwpyburn05@gmail.com>
2026-05-24 12:58:59 -0500

    Update main.c for testing initilization code.

--- src/main.c
+++ src/main.c
@@ -3,63 +3,17 @@
 #include <stdlib.h>
 #include "pico/stdlib.h"
 
-#include "src/config/flash.h"
-#include <string.h>
-
-extern char __config_reserved_flash;
+#include "src/core/init.h"
 
 int main(void) 
 {
     stdio_init_all();
 
-    config_RegisterParam("Version", sizeof(int));
-    config_RegisterParam("PID I Gain", sizeof(float));
+    while (getc(stdin) != '\r');
 
-    while (1) {
-        char c = '\0';
-        while (c != '1' && c != '2')
-            c = (char)stdio_getchar_timeout_us(100);
+    core_Init();
 
-        if (c == '1') {
-            puts("Writing parameters to flash.");
+    printf("Done...");
 
-            if (config_SetParam("Version", (int[]){16}) != EXIT_SUCCESS)
-                puts("Error: SetParam failed");
-            if (config_SetParam("PID I Gain", (float[]){1.25}) != EXIT_SUCCESS)
-                puts("Error: SetParam failed");
-
-            int *version = (int*)config_GetParam("Version");
-            float *kI = (float*)config_GetParam("PID I Gain");
-
-            if (!version)
-                puts("Error: Failed to retrieve parameter from config table");
-            else
-                printf("Version = %d (RAM)\r\n", *version);
-            if (!kI)
-                puts("Error: Failed to retrieve parameter from config table");
-            else
-                printf("PID I Gain = %f (RAM)\r\n", *kI);
-
-            config_WriteAll();
-
-        } else {
-            puts("Retrieving parameters from flash.");
-
-            config_ReadAll();
-
-            int *version = (int*)config_GetParam("Version");
-            float *kI = (float*)config_GetParam("PID I Gain");
-
-            if (!version)
-                puts("Error: Failed to retrieve parameter from config table");
-            else
-                printf("Version = %d (Flash)\r\n", *version);
-            if (!kI)
-                puts("Error: Failed to retrieve parameter from config table");
-            else
-                printf("PID I Gain = %f (Flash)\r\n", *kI);
-        }
-
-        printf("Done...\r\n");
-    }
+    return 0;
 }
