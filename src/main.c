7a166300510cfdf5d9a80e435d58142888ad8ce3
Jack Pyburn <jwpyburn05@gmail.com>
2026-05-30 18:34:33 -0500

    Update main.c for testing dshot code.

--- src/main.c
+++ src/main.c
@@ -3,17 +3,26 @@
 #include <stdlib.h>
 #include "pico/stdlib.h"
 
-#include "src/core/init.h"
+#include "src/actuators/dshot.h"
+
+#ifndef DSHOT_PIN
+#define DSHOT_PIN 5
+#endif
 
 int main(void) 
 {
+    dshot_controller_t motor1;
+
     stdio_init_all();
+    
+    dshot_ControllerInit(&motor1, 300, pio0, 0, DSHOT_PIN, 1);
+    dshot_Throttle(&motor1, 0, 0);
 
-    while (getc(stdin) != '\r');
+    while (1) {
+        while (getc(stdin) != '\r');
 
-    core_Init();
-
-    printf("Done...");
+        dshot_Command(&motor1, 0, DSHOT_CMD_BEEP1); 
+    }
 
     return 0;
 }
